73fda5dac451ceda0c7e9a19e53534b8345ad579
axmaijaz <asmaijaz456@gmail.com>
2023-10-30 12:28:36 +0500

    new TestEBS version label added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Minimal .NET Console App!");
+        
+        while (true)
+        {
+            Console.WriteLine("\nPlease select an option:");
+            Console.WriteLine("1. Say Hello");
+            Console.WriteLine("2. Exit");
+            
+            string choice = Console.ReadLine();
+            
+            switch (choice)
+            {
+                case "1":
+                    SayHello();
+                    break;
+                case "2":
+                    Console.WriteLine("Goodbye!");
+                    return; // Exit the application
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    static void SayHello()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
